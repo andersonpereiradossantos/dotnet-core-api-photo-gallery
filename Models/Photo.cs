fb01a7d4eb5dadc125c0ef0c6adcc10bfb9be50f
Anderson Pereira dos Santos <andersonpereiradossantos@outlook.com>
2021-09-27 19:20:05 -0300

    Change visibility of attributtes

--- Models/Photo.cs
+++ Models/Photo.cs
@@ -18,14 +18,14 @@ namespace PhotoInfoApi.Models
         [JsonIgnore]
         public string MimeType { get; internal set; }
         [JsonIgnore]
-        public string Hash { get; internal set; }
+        public string Hash { get; set; }
         [JsonIgnore]
         public string Extension { get; internal set; }
         public bool Cover { get; internal set; }
         [NotMapped]
         [JsonIgnore]
-        public IFormFile File { get; internal set; }
-        public long AlbumId { get; internal set; }
+        public IFormFile File { get; set; }
+        public long AlbumId { get; set; }
         [ForeignKey("AlbumId")]
         [JsonIgnore]
         public virtual Album Album { get; }
